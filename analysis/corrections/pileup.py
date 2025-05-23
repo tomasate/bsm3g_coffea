42010ca1e79733bc65378afdc22121bc4c753dd2
deoache <daniel.ocampoh@udea.edu.co>
2025-05-23 17:41:28 +0000

    update naming

--- analysis/corrections/pileup.py
+++ analysis/corrections/pileup.py
@@ -48,13 +48,13 @@ def add_pileup_weight(
         down_sf = cset[year_to_corr[year]].evaluate(ak.to_numpy(nti), "down")
         # add pileup scale factors to weights container
         weights_container.add(
-            name=f"CMS_pileup_{year}",
+            name=f"CMS_pileup",
             weight=nominal_sf,
             weightUp=up_sf,
             weightDown=down_sf,
         )
     else:
         weights_container.add(
-            name == f"CMS_pileup_{year}",
+            name == f"CMS_pileup",
             weight=nominal_sf,
         )
