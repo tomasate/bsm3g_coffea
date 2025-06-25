828d31969d0c35f33b0a62f8e7d6bbe675424d68
deoache <daniel.ocampoh@udea.edu.co>
2025-06-25 11:02:27 +0000

    update workflows for uncertainty table var

--- analysis/postprocess/utils.py
+++ analysis/postprocess/utils.py
@@ -199,7 +199,7 @@ def uncertainty_table(processed_histograms, workflow):
             to_accumulate.append(processed_histograms[process])
     helper_histo = accumulate(to_accumulate)
 
-    var = "electron_met_mass" if workflow == "2b1e" else "muon_met_mass"
+    var = "electron_met_mass" if workflow in ["2b1e", "1b1e1mu"] else "muon_met_mass"
     helper_histo = helper_histo["mass"].project(var, "variation")
 
     # get histogram per variation
